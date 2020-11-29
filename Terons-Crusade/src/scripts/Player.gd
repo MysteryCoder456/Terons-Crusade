1e6f7d5272adceb0b61c8bf4b13d69b0c8fffe0e
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-11-29 15:57:06 +0400

    added some player animations

--- Terons-Crusade/src/scripts/Player.gd
+++ Terons-Crusade/src/scripts/Player.gd
@@ -6,10 +6,12 @@ export var speed: Vector2
 
 
 func _ready():
+	$AnimatedSprite.playing = true
 	gravity = get_tree().get_root().get_node("world").gravity
 
 
 func _physics_process(delta):
+	animate_character()
 	velocity = get_movement_velocity()
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
@@ -18,7 +20,26 @@ func get_movement_velocity():
 	var movement_vector = velocity
 	movement_vector.x = (Input.get_action_strength("move_right") - Input.get_action_strength("move_left")) * speed.x
 
-#	Apply gravity
-	movement_vector.y += gravity * get_physics_process_delta_time()
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		movement_vector.y = -speed.y
+	else:
+		movement_vector.y += gravity * get_physics_process_delta_time()
 	
 	return movement_vector
+	
+	
+func animate_character():	
+	if velocity != Vector2.ZERO:
+		if velocity.x > 0:
+			$AnimatedSprite.play("running")
+			$AnimatedSprite.flip_h = false
+		elif velocity.x < 0:
+			$AnimatedSprite.play("running")
+			$AnimatedSprite.flip_h = true
+			
+		if velocity.y > 0:
+			$AnimatedSprite.play("falling")
+		elif velocity.y < 0:
+			$AnimatedSprite.play("jumping")
+	else:
+		$AnimatedSprite.play("idle")
