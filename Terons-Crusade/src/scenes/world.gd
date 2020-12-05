415f6cd59d74b002291f785d01c3c808ae7a0c95
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-12-05 23:00:54 +0400

    blocks only placeable if a placeable item held in hotbar

--- Terons-Crusade/src/scenes/world.gd
+++ Terons-Crusade/src/scenes/world.gd
@@ -6,16 +6,21 @@ const DIRECTIONS = [Vector2.DOWN, Vector2.LEFT, Vector2.RIGHT, Vector2.UP]
 
 func _input(event):
 	if !player.inventory_open:
+		var player_held_item = Globals.player_hotbar[player.current_hotbar_selection]
 		var mouse_pos = get_global_mouse_position()
-		var block_id = 2
-		
+				
 		if mouse_pos.distance_to(player.global_position) <= player.reach_distance:
 			if Input.is_action_just_pressed("use"):
-				var block_position = $TileMap.world_to_map(mouse_pos)
-				
-				if $TileMap.get_cellv(block_position) == $TileMap.INVALID_CELL:
-					if is_block_nearby(block_position):
-						$TileMap.set_cellv(block_position, block_id)
+				if player_held_item:
+					var item_info = JsonData.item_data[player_held_item[0]]
+					
+					if item_info["category"] == "block":
+						var block_id = item_info["block_id"]
+						var block_position = $TileMap.world_to_map(mouse_pos)
+						
+						if $TileMap.get_cellv(block_position) == $TileMap.INVALID_CELL:
+							if is_block_nearby(block_position):
+								$TileMap.set_cellv(block_position, block_id)
 				
 			elif Input.is_action_just_pressed("break"):
 				var block_position = $TileMap.world_to_map(mouse_pos)
