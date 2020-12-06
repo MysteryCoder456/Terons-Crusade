1e84bbc3b38d0f54294405c972afa737caee934c
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-12-06 13:27:47 +0400

    added pickaxe sprite

--- Terons-Crusade/src/scenes/world.gd
+++ Terons-Crusade/src/scenes/world.gd
@@ -9,22 +9,24 @@ func _input(event):
 		var player_held_item = Globals.player_hotbar[player.current_hotbar_selection]
 		var mouse_pos = get_global_mouse_position()
 				
-		if mouse_pos.distance_to(player.global_position) <= player.reach_distance:
-			if Input.is_action_just_pressed("use"):
-				if player_held_item:
-					var item_info = JsonData.item_data[player_held_item[0]]
+		if player_held_item:
+			var item_info = JsonData.item_data[player_held_item[0]]
 					
-					if item_info["category"] == "block":
-						var block_id = item_info["block_id"]
+			if item_info["category"] == "block":
+				var block_id = item_info["block_id"]
+			
+				if mouse_pos.distance_to(player.global_position) <= player.reach_distance:
+					if Input.is_action_just_pressed("use"):
 						var block_position = $TileMap.world_to_map(mouse_pos)
 						
 						if $TileMap.get_cellv(block_position) == $TileMap.INVALID_CELL:
 							if is_block_nearby(block_position):
 								$TileMap.set_cellv(block_position, block_id)
-				
-			elif Input.is_action_just_pressed("break"):
-				var block_position = $TileMap.world_to_map(mouse_pos)
-				$TileMap.set_cellv(block_position, $TileMap.INVALID_CELL)
+								
+			elif item_info["category"] == "tool":
+				if Input.is_action_just_pressed("break"):
+					var block_position = $TileMap.world_to_map(mouse_pos)
+					$TileMap.set_cellv(block_position, $TileMap.INVALID_CELL)
 			
 			
 func is_block_nearby(block_pos) -> bool:
