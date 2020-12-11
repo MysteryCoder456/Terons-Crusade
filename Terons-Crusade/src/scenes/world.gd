0f4e526550eae343b8ba5afce654d2a3106bbdd6
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-12-12 03:09:30 +0400

    added tool type requirements for block breaking

--- Terons-Crusade/src/scenes/world.gd
+++ Terons-Crusade/src/scenes/world.gd
@@ -13,10 +13,10 @@ func _input(event):
 			var item_info = JsonData.item_data[player_held_item[0]]
 					
 			if item_info["category"] == "block":
-				var block_id = item_info["block_id"]
+				if Input.is_action_just_pressed("use"):
+					var block_id = item_info["block_id"]
 			
-				if mouse_pos.distance_to(player.global_position) <= player.reach_distance:
-					if Input.is_action_just_pressed("use"):
+					if mouse_pos.distance_to(player.global_position) <= player.reach_distance:
 						var block_position = $TileMap.world_to_map(mouse_pos)
 						
 						if $TileMap.get_cellv(block_position) == $TileMap.INVALID_CELL:
@@ -35,7 +35,13 @@ func _input(event):
 			elif item_info["category"] == "tool":
 				if Input.is_action_just_pressed("break"):
 					var block_position = $TileMap.world_to_map(mouse_pos)
-					$TileMap.set_cellv(block_position, $TileMap.INVALID_CELL)
+					var cell_id = $TileMap.get_cellv(block_position)
+					
+					if cell_id != $TileMap.INVALID_CELL:
+						var tool_type_required = JsonData.item_data[$TileMap.tile_set.tile_get_name(cell_id)]["tool_type_required"]
+						
+						if tool_type_required == item_info["tool_type"]:
+							$TileMap.set_cellv(block_position, $TileMap.INVALID_CELL)
 			
 			
 func is_block_nearby(block_pos) -> bool:
